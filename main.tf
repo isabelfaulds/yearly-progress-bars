e5dbca3ab5320139a2ecb3210e4739c9d9d0cf77
Isabel Faulds <fauldsisabel@outlook.com>
2024-12-31 13:23:18 -0800

    Fixes in tf s3 public access

--- main.tf
+++ main.tf
@@ -14,19 +14,8 @@ data "aws_route53_zone" "progress_bars_domain" {
   name = "year-progress-bar.com"
 }
 
-
-resource "aws_s3_bucket" "progress_bars_site_bucket" {
-  bucket = "year-progress-bar.com"  # Replace with your desired bucket name
-  acl    = "public-read"  # publick site
-
-  website {
-    index_document = "index.html"
-    # error_document = "error.html"
-  }
-
-    versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket" "pbars_site_bucket" {
+  bucket = "year-progress-bar.com" 
 
   tags = {
     Name    = "Progress Bars Site Bucket"
@@ -34,6 +23,56 @@ resource "aws_s3_bucket" "progress_bars_site_bucket" {
   }
 }
 
+# Disable Block Public Access for the S3 Bucket
+resource "aws_s3_bucket_public_access_block" "pbars_site_public_access" {
+  bucket = aws_s3_bucket.pbars_site_bucket.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Bucket Policy for Public Read Access
+resource "aws_s3_bucket_policy" "pbars_site_bucket_policy" {
+  bucket = aws_s3_bucket.pbars_site_bucket.bucket
+
+  policy = data.aws_iam_policy_document.pbars_site_bucket_policy.json
+}
+
+# IAM Policy Document for Public Access
+data "aws_iam_policy_document" "pbars_site_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::year-progress-bar.com/*"]
+
+    # Allow public access to the bucket
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "pbars_website" {
+  bucket = aws_s3_bucket.pbars_site_bucket.bucket
+
+  index_document {
+    suffix = "index.html"  # default page
+  }
+
+#  error_document {
+#    key = "error.html"  # TODO: 404 page
+#  }
+}
+
+resource "aws_s3_bucket_versioning" "pbars_versioning" {
+  bucket = aws_s3_bucket.pbars_site_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"  # You can also set this to "Suspended" to disable versioning
+  }
+}
 
 
 # Cloudfront ACM Certificates live in us-east-1
@@ -58,7 +97,7 @@ resource "aws_acm_certificate" "progress_bars_site_cert" {
 resource "aws_route53_record" "pbars_root_cert_validation" {
 depends_on = [aws_acm_certificate.progress_bars_site_cert]
 
-  zone_id = data.aws_route53_zone.personal_domain.zone_id
+  zone_id = data.aws_route53_zone.progress_bars_domain.zone_id
   name    = tolist(aws_acm_certificate.progress_bars_site_cert.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.progress_bars_site_cert.domain_validation_options)[0].resource_record_type
   ttl     = "60"
@@ -69,7 +108,7 @@ depends_on = [aws_acm_certificate.progress_bars_site_cert]
 resource "aws_route53_record" "pbars_www_cert_validation" {
   depends_on = [aws_acm_certificate.progress_bars_site_cert]
 
-  zone_id = data.aws_route53_zone.personal_domain.zone_id
+  zone_id = data.aws_route53_zone.progress_bars_domain.zone_id
   name    = tolist(aws_acm_certificate.progress_bars_site_cert.domain_validation_options)[1].resource_record_name
   type    = tolist(aws_acm_certificate.progress_bars_site_cert.domain_validation_options)[1].resource_record_type
   ttl     = "60"
@@ -100,14 +139,16 @@ resource "aws_cloudfront_distribution" "pbars_cloudfront" {
   default_cache_behavior {
     target_origin_id = "S3-year-progress-bar"
     viewer_protocol_policy = "redirect-to-https"
-    allowed_methods {
-      items = ["GET", "HEAD"]
-      cached_methods {
-        items = ["GET", "HEAD"] 
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
       }
     }
-    forward_cookies = "none"
-    forward_query_string = false
+
     min_ttl = 0  # Minimum TTL for caching
     default_ttl = 86400  # Default TTL for caching (1 day)
     max_ttl = 86400  # Maximum TTL for caching
