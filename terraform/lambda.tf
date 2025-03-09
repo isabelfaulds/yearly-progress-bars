8c9f90c4ef32eea09b3519799942a9384f146cef
Isabel Faulds <fauldsisabel@outlook.com>
2025-03-09 16:40:36 -0700

    Merge branch 'main' into auth-gateway-authorizer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -103,7 +103,7 @@ resource "aws_lambda_permission" "allow_apigateway_invocation" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.node_auth_token_creation.arn
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:us-west-1:***REMOVED***:vae1x9x8se/*/POST/users_auth"
+  source_arn    = "arn:aws:execute-api:us-west-1:${data.aws_caller_identity.current.account_id}:vae1x9x8se/*/POST/users_auth"
 }
 
 
@@ -131,5 +131,125 @@ resource "aws_lambda_permission" "allow_apigateway_invocation_invalidation" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.node_auth_token_invalidation.arn
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:us-west-1:***REMOVED***:vae1x9x8se/*/POST/users_auth/logout"
+  source_arn    = "arn:aws:execute-api:us-west-1:${data.aws_caller_identity.current.account_id}:vae1x9x8se/*/POST/users_auth/logout"
 }
+
+
+### token refresh
+resource "aws_s3_bucket_object" "node_auth_token_refresh" {
+  bucket = aws_s3_bucket.pbars_lambdas_bucket.bucket
+  source = "../backend/auth-token-refresh/auth-token-refresh.zip"
+  key    = "auth-token-refresh.zip"
+  content_type  = "application/zip"
+}
+
+resource "aws_lambda_function" "node_auth_token_refresh" {
+  function_name = "node-auth-token-refresh"
+  s3_bucket     = aws_s3_bucket.pbars_lambdas_bucket.bucket
+  s3_key        = aws_s3_bucket_object.node_auth_token_refresh.key
+
+  handler = "index.handler"
+  runtime = "nodejs22.x"  
+  depends_on = [aws_s3_bucket_object.node_auth_token_refresh]
+
+
+  role = aws_iam_role.lambda_execution_role.arn
+  timeout = 100
+  memory_size = 128
+
+  environment {
+    variables = {
+        JWT_SECRET = var.jwt_secret
+    }
+  }
+}
+
+resource "aws_lambda_permission" "allow_apigateway_invocation_refresh" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.node_auth_token_refresh.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:us-west-1:${data.aws_caller_identity.current.account_id}:vae1x9x8se/*/POST/users_auth/refresh"
+}
+
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name = "/aws/lambda/node-auth-token-refresh"
+  retention_in_days = 7
+}
+
+### token authorizer
+resource "aws_s3_bucket_object" "node_auth_token_authorizer" {
+  bucket = aws_s3_bucket.pbars_lambdas_bucket.bucket
+  source = "../backend/auth-token-authorizer/auth-token-authorizer.zip"
+  key    = "auth-token-authorizer.zip"
+  content_type  = "application/zip"
+}
+
+resource "aws_lambda_function" "node_auth_token_authorizer" {
+  function_name = "node-auth-token-authorizer"
+  s3_bucket     = aws_s3_bucket.pbars_lambdas_bucket.bucket
+  s3_key        = aws_s3_bucket_object.node_auth_token_authorizer.key
+
+  handler = "index.handler"
+  runtime = "nodejs22.x"  
+  depends_on = [aws_s3_bucket_object.node_auth_token_authorizer]
+
+
+  role = aws_iam_role.lambda_execution_role.arn
+  timeout = 100
+  memory_size = 128
+
+  environment {
+    variables = {
+        JWT_SECRET = var.jwt_secret
+    }
+  }
+}
+
+resource "aws_lambda_permission" "allow_apigateway_invocation_authorizer" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.node_auth_token_refresh.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:us-west-1:${data.aws_caller_identity.current.account_id}:vae1x9x8se/*/POST/*/*"
+}
+
+
+
+# resource "aws_lambda_permission" "api_gateway_auth" {
+#   statement_id  = "AllowAPIGatewayInvoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = "node-auth-token-authorizer"
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${aws_api_gateway_rest_api.your_api.execution_arn}/*/*"
+# }
+
+# #### spi gateway authorizer
+
+# resource "aws_api_gateway_authorizer" "token_authorizer" {
+#   name            = "TokenAuthorizer"
+#   rest_api_id     = aws_api_gateway_rest_api.your_api.id # Replace with your API Gateway ID
+#   authorizer_uri  = "arn:aws:lambda:us-west-1:${data.aws_caller_identity.current.account_id}:function:node-auth-token-authorizer" 
+#   type            = "TOKEN"
+#   identity_source = "method.request.header.Authorization" # Or other source (e.g., query string)
+
+#   # Optional settings:
+#   authorizer_credentials = aws_iam_role.api_gateway_authorizer_role.arn # If you need credentials
+#   authorizer_result_ttl_in_seconds = 300 # Token caching TTL
+# }
+
+# resource "aws_iam_role" "api_gateway_authorizer_role" {
+#   name = "api-gateway-authorizer-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "apigateway.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
+
