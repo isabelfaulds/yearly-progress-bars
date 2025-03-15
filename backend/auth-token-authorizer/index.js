4dc6a09d390ac6f5a82f97d3d79c602a2baef569
Isabel Faulds <fauldsisabel@outlook.com>
2025-03-15 14:57:26 -0700

    Changed allowed arn for authorizer

--- backend/auth-token-authorizer/index.js
+++ backend/auth-token-authorizer/index.js
@@ -19,21 +19,32 @@ function generatePolicy(principalId, effect, resource) {
 
 exports.handler = async (event) => {
   const accessToken = event.headers["login-auth-token"];
+  // Broader arn for less invocations
+  const methodArn = event.methodArn;
+  const arnParts = methodArn.split(":");
+  const apiGatewayArn = arnParts[5].split("/");
+  const region = arnParts[3];
+  const accountId = arnParts[4];
+  const apiId = apiGatewayArn[0];
+  const stage = apiGatewayArn[1];
   try {
     const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
-    console.log("User ID from JWT:", userId);
+    console.log(
+      "Permitting",
+      `arn:aws:execute-api:${region}:${accountId}:${apiId}/${stage}/*/*`
+    );
     return generatePolicy(
       userId,
       "Allow",
-      `arn:aws:execute-api:${event.region}:${event.accountId}:${event.apiId}/${event.stage}/*/*`
+      `arn:aws:execute-api:${region}:${accountId}:${apiId}/${stage}/*/*`
     );
   } catch (error) {
     console.error("JWT verification failed:", error);
     return generatePolicy(
       "user",
       "Deny",
-      `arn:aws:execute-api:${event.region}:${event.accountId}:${event.apiId}/${event.stage}/*/*`
+      `arn:aws:execute-api:${region}:${accountId}:${apiId}/${stage}/*/*`
     );
   }
 };
