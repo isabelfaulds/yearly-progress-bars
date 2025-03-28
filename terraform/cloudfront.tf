47ec53e41aa5fac53ae5a77684c50412536a5848
Isabel Faulds <fauldsisabel@outlook.com>
2025-03-27 17:12:01 -0700

    adding route53 cname & certs for api subdomain

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -161,3 +161,35 @@ resource "aws_cloudfront_distribution" "api_cloudfront_distribution" {
   }
 }
 
+
+resource "aws_route53_record" "cloudfront_cname" {
+  zone_id = data.aws_route53_zone.progress_bars_domain.zone_id 
+  name    = "api.year-progress-bar.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_cloudfront_distribution.api_cloudfront_distribution.domain_name]
+}
+
+resource "aws_acm_certificate" "api_subdomain_cert" {
+  provider          = aws.us_east_1
+  domain_name       = "api.year-progress-bar.com"
+  validation_method = "DNS"
+}
+
+
+resource "aws_route53_record" "api_subdomain_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api_subdomain_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.progress_bars_domain.zone_id 
+}
