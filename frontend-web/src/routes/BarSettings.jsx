4525b3e2504bf1a4004b6ae2137e530d343b05d4
Isabel Faulds <fauldsisabel@outlook.com>
2025-03-31 11:22:31 -0700

    feat(day-view) : renamed settings route for routing

--- frontend-web/src/routes/BarSettings.jsx
+++ frontend-web/src/routes/BarSettings.jsx
@@ -1,23 +1,16 @@
 import { useState } from "react";
 import TimePicker from "../components/TimePicker";
 import { XMarkIcon, ArrowRightCircleIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from "react-router-dom";
 
 const Settings = () => {
+  const navigate = useNavigate();
   const [dayEndHourSetting, dayEndHourSettingValue] = useState(
     localStorage.getItem("dayEndHourSetting") || "11:59 PM"
   );
-  const handleEndSave = (time) => {
-    localStorage.setItem("dayEndHourSetting", time);
-    console.log("update end ", localStorage.getItem("dayEndHourSetting"));
-  };
-
   const [dayStartHourSetting, dayStartHourSettingValue] = useState(
     localStorage.getItem("dayStartHourSetting") || "12:00 AM"
   );
-  const handleStartSave = (time) => {
-    localStorage.setItem("dayStartHourSetting", time);
-    console.log("update start ", localStorage.getItem("dayStartHourSetting"));
-  };
 
   return (
     <div className="bg-[#f2f5f4] bg-cover bg-center w-screen min-h-screen m-0 flex flex-col items-start pt-10 pl-1 sm:pt-12 sm:pl-20 px-4 sm:px-20">
@@ -25,7 +18,7 @@ const Settings = () => {
         Settings
       </div>
       <button
-        onClick={() => (window.location.href = "/")}
+        onClick={() => navigate(-1)}
         className="fixed top-7 right-4 p-3 text-white rounded-full rounded-full shadow-lg focus:outline-none hover:border-2"
       >
         <XMarkIcon className="text-gray-700 h-6 w-6" />
@@ -41,14 +34,18 @@ const Settings = () => {
           </label>
           <TimePicker
             defaultTime={dayStartHourSetting}
-            onTimeSelect={(time) => handleStartSave(time)}
+            onTimeSelect={(time) =>
+              localStorage.setItem("dayStartHourSetting", time)
+            }
           />
           <label className="block text-gray-600 sm:text-lg font-semibold mb-2">
             End of Day
           </label>
           <TimePicker
             defaultTime={dayEndHourSetting}
-            onTimeSelect={(time) => handleEndSave(time)}
+            onTimeSelect={(time) =>
+              localStorage.setItem("dayEndHourSetting", time)
+            }
           />
         </div>
       </div>
