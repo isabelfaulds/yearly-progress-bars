be52143b15a0ab0a5bdd6e301741909fc5058fb2
Isabel Faulds <fauldsisabel@outlook.com>
2025-05-24 17:11:22 -0700

    feat(range-view) daypicker component

---
+++ frontend-web/src/components/DayPicker.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { addDays } from "date-fns";
+
+const CustomDayPicker = ({ onRangeChange, initialRange }) => {
+  const [range, setRange] = useState(initialRange);
+
+  // Default 7 days before today
+  const defaultSelected = {
+    from: addDays(new Date(), -7),
+    to: new Date(),
+  };
+
+  const selectedRange = range || defaultSelected;
+
+  //  Date Selection Handler
+  const handleRangeSelect = (newRange) => {
+    setRange(newRange);
+    if (onRangeChange) {
+      onRangeChange(newRange); // parent update
+    }
+  };
+
+  return (
+    <div className="bg-gray-900 p-4 rounded-lg shadow-lg flex flex-col items-center justify-center">
+      <DayPicker
+        animate
+        mode="range"
+        selected={selectedRange}
+        onSelect={handleRangeSelect}
+        captionLayout="dropdown"
+        startMonth={new Date(2025, 0)}
+        endMonth={new Date()}
+        classNames={{
+          today: "font-bold text-blue-400",
+          day: "rdp-day hover:bg-blue-300 hover:rounded-full",
+          selected: "hover:!rounded-none text-black",
+        }}
+      />
+    </div>
+  );
+};
+
+export default CustomDayPicker;
